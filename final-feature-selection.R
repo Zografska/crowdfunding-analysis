a52bab12f362ea5d64b6bfa7623144d123631c4b
Aleksandra Zografska <aleksandrazografska@codechem.com>
2025-03-21 15:51:43 +0100

    finish with trees and glm

--- final-feature-selection.R
+++ final-feature-selection.R
@@ -5,7 +5,7 @@ library(pROC)
 summary(dataset)
 
 regression_variables <- dataset %>% dplyr::select(success_status, is_donation, is_all_or_nothing,project_duration_days,
-                                           promo_video_length,has_faq,has_website, has_social_media, have_followers, 
+                                           promo_video_length,frequently_asked_questions_numb,has_website, has_social_media, have_followers, 
                                            log_social_media_followers, description_word_count,is_fongogo, in_big_city, has_female_owner, log_funding_target)
 dim(regression_variables)
 mod<-glm(success_status ~., data = regression_variables, family=binomial(link='logit'))
@@ -21,30 +21,52 @@ full_model <- glm(success_status ~ ., data = regression_variables, family = bino
 # variables - the dataset
 # is_youden - if true, calculate youden index, else euclidian index
 
-fit_and_diagnose <- function(variables, is_youden = TRUE, step_direction = "both"){
+diagnose_confusion_matrix <- function(confusion_matrix){
+  sensitivity <- confusion_matrix[1,1] / (confusion_matrix[1,1] + confusion_matrix[2,1])
+  specificity <- confusion_matrix[2,2] / (confusion_matrix[2,2] + confusion_matrix[1,2])
+  percision <- confusion_matrix[1,1] / (confusion_matrix[1,1] + confusion_matrix[1,2])
+  return(c(sensitivity, specificity, percision))
+}
+
+fit_glm <- function(variables, step_direction = "both"){
   model <- glm(success_status ~ ., data = variables, family = binomial(link='logit'))
   best_model <- MASS::stepAIC(model, direction = step_direction, trace = FALSE)
-  pred_probs <- predict(best_model, type = "response")
-  roc_curve <- roc(variables$success_status, pred_probs)
+  return(list(model=best_model))
+}
+
+predict_glm <- function(model, data, is_youden = TRUE){
+  pred_probabilities <- predict(model, newdata=data, type = "response")
+  roc_curve <- roc(data$success_status, pred_probabilities)
   plot(roc_curve, col = "blue", main = "ROC Curve")
   index <- ifelse(is_youden, which.max(roc_curve$sensitivities + roc_curve$specificities - 1),
                   which.min(sqrt((1 - roc_curve$sensitivities)^2 + (1 - roc_curve$specificities)^2)) ) 
   cutoff <- roc_curve$thresholds[index]
-  pred_classes <- ifelse(pred_probs <= cutoff, 1, 0)
+  pred_classes <- ifelse(pred_probabilities <= cutoff, 1, 0)
   pred_classes <- factor(pred_classes, levels=c(1,0))
-  confusion_matrix <- table(Predicted = pred_classes, Actual = variables$success_status)
-  sensitivity <- confusion_matrix[1,1] / (confusion_matrix[1,1] + confusion_matrix[2,1])
-  specificity <- confusion_matrix[2,2] / (confusion_matrix[2,2] + confusion_matrix[1,2])
-  percision <- confusion_matrix[1,1] / (confusion_matrix[1,1] + confusion_matrix[1,2])
-  results <- c(sensitivity, specificity, percision)
   
-  return(list(results = results, model = best_model, roc_curve = roc_curve))
+  return(list(cutoff=cutoff, predicitons=pred_classes, roc_curve=roc_curve))
 }
 
-youden_model <- fit_and_diagnose(regression_variables, is_youden = TRUE)
-euclidian_model <- fit_and_diagnose(regression_variables, is_youden = FALSE)
+set.seed(101)
+train.idx <- sample(nrow(regression_variables), 4/5 * nrow(regression_variables)) 
+data.train <- regression_variables[train.idx, ]
+data.test <- regression_variables[-train.idx, ]
 
-comparison_table <- rbind(youden_model$results, euclidian_model$results)
+youden_model <- fit_glm(data.train)
+youden_predictions <- predict_glm(youden_model$model, data.test, is_youden = TRUE)
+youden_confusion_matrix <- table(Predicted = youden_predictions$predicitons, Actual = data.test$success_status)
+youden_results <- diagnose_confusion_matrix(youden_confusion_matrix)
+youden_confusion_matrix
+youden_results
+
+euclidean_model <- fit_glm(data.train)
+euclidean_predictions <- predict_glm(euclidean_model$model, data.test, is_youden = FALSE)
+euclidean_confusion_matrix <- table(Predicted = euclidean_predictions$predicitons, Actual = data.test$success_status)
+euclidean_results <- diagnose_confusion_matrix(euclidean_confusion_matrix)
+euclidean_confusion_matrix
+euclidean_results
+
+comparison_table <- rbind(youden_results, euclidean_results)
 colnames(comparison_table) <- c("Sensitivity", "Specificity", "Percision")
 rownames(comparison_table) <- c("Youden", "Euclidian")
 comparison_table
@@ -60,22 +82,52 @@ sqrt(car::vif(best_model))
 high_vif <- which(sqrt(car::vif(best_model)) > 2)
 high_vif
 
-cleaned_variables <- regression_variables %>% dplyr::select(-c(has_social_media))
-cleaned_model_youden <- fit_and_diagnose(cleaned_variables, is_youden = TRUE)
-cleaned_model_euclidian <- fit_and_diagnose(cleaned_variables, is_youden = FALSE)
+cleaned.data.train <- data.train %>% dplyr::select(-c(has_social_media))
+cleaned.data.test <- data.test %>% dplyr::select(-c(has_social_media))
+
+cleaned_youden_model <- fit_glm(cleaned.data.train)
+cleaned_youden_predictions <- predict_glm(cleaned_youden_model$model, cleaned.data.test, is_youden = FALSE)
+cleaned_youden_confusion_matrix <- table(Predicted = cleaned_youden_predictions$predicitons, Actual = cleaned.data.test$success_status)
+cleaned_youden_results <- diagnose_confusion_matrix(cleaned_youden_confusion_matrix)
+cleaned_youden_confusion_matrix
+cleaned_youden_results
+
+cleaned_euclidean_model <- fit_glm(cleaned.data.train)
+cleaned_euclidean_predictions <- predict_glm(cleaned_euclidean_model$model, cleaned.data.test, is_youden = FALSE)
+cleaned_euclidean_confusion_matrix <- table(Predicted = cleaned_euclidean_predictions$predicitons, Actual = cleaned.data.test$success_status)
+cleaned_euclidean_results <- diagnose_confusion_matrix(cleaned_euclidean_confusion_matrix)
+cleaned_euclidean_confusion_matrix
+cleaned_euclidian_results
 
 
-table <- rbind(youden_model$results, cleaned_model_youden$results, cleaned_model_euclidian$results)
-colnames(table) <- c("Sensitivity", "Specificity", "Percision")
-rownames(table) <- c("Youden", "Cleaned Youden", "Cleaned Euclidian")
-table
-# the sensitivity improved for the cleaned model, but not the specificity and percision
+comparison_table <- rbind(youden_results, cleaned_youden_results, cleaned_euclidian_results)
+colnames(comparison_table) <- c("Sensitivity", "Specificity", "Percision")
+rownames(comparison_table) <- c("Youden", "Cleaned Youden", "Cleaned Euclidian")
+comparison_table
+# the specificity and percision improved for the cleaned model, but not the sensitity 
 
 # compare auc
-auc(youden_model$roc_curve)
-auc(cleaned_model_youden$roc_curve)
-# the auc is a bit lower for the cleaned model
+auc(euclidean_predictions$roc_curve)
+auc(cleaned_euclidean_predictions$roc_curve)
+# the auc is the same
 
-plot(youden_model$roc_curve, col = "blue", main = "ROC Curve", lwd = 4)
-lines(cleaned_model_youden$roc_curve, col = "red")
-legend("bottomright", legend = c("Full Model", "Cleaned Model"), col = c("blue", "red"), lwd = 4)
+# what if we change the step direction?
+cleaned_model_youden_backward <- fit_and_diagnose(cleaned_variables, is_youden = TRUE, step_direction = "backward")
+backward_model <- fit_glm(cleaned.data.train, step_direction = "backward")
+backward_predictions <- predict_glm(backward_model$model, cleaned.data.test, is_youden = FALSE)
+backward_confusion_matrix <- table(Predicted = backward_predictions$predicitons, Actual = cleaned.data.test$success_status)
+backward_results <- diagnose_confusion_matrix(backward_confusion_matrix)
+backward_confusion_matrix
+
+forward_model <- fit_glm(cleaned.data.train, step_direction = "forward")
+forward_predictions <- predict_glm(forward_model$model, cleaned.data.test, is_youden = FALSE)
+forward_confusion_matrix <- table(Predicted = forward_predictions$predicitons, Actual = cleaned.data.test$success_status)
+forward_results <- diagnose_confusion_matrix(forward_confusion_matrix)
+forward_confusion_matrix
+
+table <- rbind(youden_results, forward_results, backward_results)
+colnames(table) <- c("Sensitivity", "Specificity", "Percision")
+rownames(table) <- c("Backward", "Forward", "Both")
+table
+# the forward selection gives better results
+
--- final_trees.R
+++ final_trees.R
@@ -2,10 +2,12 @@ library("rpart")
 library("rpart.plot")
 library("party") 
 library("partykit")
+library("dplyr")
 # splitting
 set.seed(101)
-for_tree <- dataset %>% dplyr::select(success_status, project_duration_days, has_faq, num_comments,team_size, num_social_followers, num_tags, description_word_count, funding_collected, in_big_city)
-
+for_tree <- dataset %>% dplyr::select(success_status, is_donation, is_all_or_nothing,project_duration_days,
+                                                  promo_video_length,frequently_asked_questions_numb,has_website, has_social_media, have_followers, 
+                                                  log_social_media_followers, description_word_count,is_fongogo, in_big_city, has_female_owner, log_funding_target)
 
 train.idx <- sample(nrow(for_tree), 2/3 * nrow(for_tree)) 
 data.train <- for_tree[train.idx, ]
@@ -20,3 +22,56 @@ rtree<-rpart(success_status ~ ., data=data.train, cp=0.005)
 printcp(rtree)
 plotcp(rtree)
 summary(rtree)
+
+
+## from group proj
+
+
+set.seed(101)
+# after a couple of tries, best split for the balance between the validation error and relational error
+train.idx <- sample(nrow(for_tree), 4/5 * nrow(for_tree)) 
+data.train <- for_tree[train.idx, ]
+data.test <- for_tree[-train.idx, ]
+
+pptree<-ctree(success_status ~ ., data=data.train, control = ctree_control(mincriterion=0.01, minsplit=0, minbucket=10))
+plot(pptree)
+# tree is chaotic, there is a lot of splits
+# need a smaller tree
+
+
+dim(data.train)
+rtree<-rpart(success_status ~ ., data=data.train, cp=0.0001)
+printcp(rtree)
+
+# rel error
+# As splits are added, 
+# the relative error decreases, but at a point, the cv error increases also
+# we need to stop at 17 where cv error is one of the lowest, 
+# and rel error seems to stabilize and also the split is still a smaller number than the others
+
+
+# xerror
+# This is the error measured using cross-validation,
+# which helps estimate how well the tree generalizes to unseen data.
+
+# it looks like it stabilazes around 17 splits, so I prune at that cp value
+pruned_tree <- prune(rtree, cp = 0.0065574)
+rpart.plot(pruned_tree)
+
+# do predictions with pruned tree
+tree_predictions <- predict(pruned_tree, data.test, type = "class")
+tree_confusion_matrix <- table(Predicted = tree_predictions, Actual = data.test$success_status)
+tree_test_results <- diagnose_confusion_matrix(tree_confusion_matrix)
+
+glm_model <- fit_glm(data.train, step_direction = "forward")
+glm_predictions <- predict_glm(glm_model$model, data.test, is_youden = FALSE)
+glm_confusion_matrix <- table(Predicted = glm_predictions$predicitons, Actual = data.test$success_status)
+glm_results <- diagnose_confusion_matrix(glm_confusion_matrix)
+glm_confusion_matrix
+
+comparison_table <- rbind(tree_test_results, glm_results)
+colnames(comparison_table) <- c("Sensitivity", "Specificity", "Percision")
+rownames(comparison_table) <- c("Tree", "GLM")
+comparison_table
+# the GLM has higher sensitivity, but the tree has higher specificity and percision
+
